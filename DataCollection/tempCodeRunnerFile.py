c0b502d3ee1d1b7b9ad036379ad9284eb0d96eb3
Yiwei_Zhu <yzhu112@jhu.edu>
2022-04-15 12:47:28 -0400

    upload the data

---
+++ DataCollection/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+'W'", "'S'", "'D'", "'E'", "'Q'", "'Z'", Key.ctrl_l, Key.shift]
+log = {'movement': 0, 'useless': 0, 'begin_time':time(), 'label':1}
--- DataCollection/test.py
+++ DataCollection/test.py
@@ -2,10 +2,11 @@ from time import sleep
 from win32gui import GetWindowText, GetForegroundWindow
 from time import time
 from pynput.keyboard import Key, Listener
+from pynput.mouse import Listener as mouseListener
 from pyautogui import position
 import threading
 
-timeGap = 5
+timeGap = 30
 mouseSleep = 0.1
 timerOn = True
 movement_key = ["'a'", "'w'", "'s'", "'d'", "'e'", "'q'", "'z'", "'A'", "'W'", "'S'", "'D'", "'E'", "'Q'", "'Z'", Key.ctrl_l, Key.shift]
@@ -13,6 +14,8 @@ log = {'movement': 0, 'useless': 0, 'begin_time':time()}
 start = time()
 mouseRecord = []
 lastMousePos = position()
+
+filename = 'cheat.log'
 
 
 def on_press(key):
@@ -24,15 +27,31 @@ def on_press(key):
 def on_release(key):
     global log
     global timerOn
-    # print('{0} release'.format(key))
+    print('{0} release'.format(key))
     if (str(key) in movement_key) or (key in movement_key):
         log['movement']+=1
     else:
         log['useless']+=1
-    if key == Key.esc:
-        # Stop listener
-        timerOn = False
-        return False
+    # if key == Key.esc:
+    #     # Stop listener
+    #     timerOn = False
+    #     return False
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    # if not pressed:
+    #     # Stop listener
+    #     return False
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0}'.format(
+        (x, y)))
 
 
 def timer():
@@ -45,6 +64,9 @@ def timer():
         log['keys_per_min'] = (log['movement'] + log['useless']) * 60 / timeGap
         log['speed_of_mouse_moved'] = sum(mouseRecord)/len(mouseRecord)
         mouseRecord = []
+        f = open(filename, 'a')
+        f.write(str(log) + '\n')
+        f.close()
         print(log)
         log = {'movement': 0, 'useless': 0, 'begin_time':time()}
 
@@ -68,9 +90,14 @@ mouseTmr.start()
 
 # Collect events until release2312233
 with Listener(
-        on_press=on_press,
+        # on_press=on_press,
         on_release=on_release) as listener:
     listener.join()
+
+# with mouseListener(
+#         # on_move=on_move,
+#         on_click=on_click) as anotherlistener:
+#     anotherlistener.join()
 
 tmr.join()
 mouseTmr.join()
